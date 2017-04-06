f7c3d812a8b94292cf2b6934c5740a5010547893
young.huang <young.huang@jfz.com>
2017-04-06 15:38:27 +0800

    feat : feat

--- _posts/share/demo/XcodeProjectDemo/XcodeProjectDemo/ViewController.swift
+++ _posts/share/demo/XcodeProjectDemo/XcodeProjectDemo/ViewController.swift
@@ -10,18 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var num1: Int?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let image = UIImage(named: "chat")
-        let imageView = UIImageView(image: image)
-        view.addSubview(imageView)
-        imageView.frame = CGRect(x: 0, y: 50, width: 200, height: 150)
+        let button = UIButton(type: UIButtonType.custom)
+        button.setTitle("按钮", for: UIControlState.normal)
+        button.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(buttonAction(btn:)), for: UIControlEvents.touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func buttonAction(btn: UIButton) {
+        print("按钮事件")
+        let vc = UIViewController()
+        self.show(vc, sender: nil)
     }
     
     func AmIHansome() -> Bool {
